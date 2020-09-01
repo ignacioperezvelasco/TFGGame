1cab291928eb15e34bb8732b67718b3b2df9c787
Ignacio <ignacioperezvelasco12@gmail.com>
2020-09-01 11:23:26 +0200

    Agent done and starting player
    maybe need few changes in the general organitzation

---
+++ TFGGame/Assets/Scenes/Scripts/Agents/Agent.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Agent : MonoBehaviour
+{
+    //variables
+    public int life;
+    public float speed;
+    //methods
+    public void GetHit() { }
+    public void Death() { }
+}
---
+++ TFGGame/Assets/Scenes/Scripts/Agents/Weapons.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapons : MonoBehaviour
+{
+    enum weaponType { SWORD }
+
+    weaponType type;
+}
